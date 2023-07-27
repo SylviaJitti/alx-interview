8e90c6cf8604c64cd7631319bf50dc03ff891bf6
SylviaJitti <sylviajitti@gmail.com>
2023-07-27 22:33:37 +0100

    method that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
-"""
-    UTF-8 Validation
-"""
+""" determines if a given data set represents a valid UTF-8 encoding """
 
 
 def validUTF8(data):
-    """ Validate UTF-8 Encodings """
-    num_bytes = 0
+    # checks if most significant byte is 1
+    mask1 = 1 << 7
+    # checks if second most significant byte is 0
+    mask2 = 1 << 6
+    # keeps track of how many 1s before 0 occurs
+    n_bytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
     for num in data:
-        num = format(num, '#010b')[-8:]
+        mask = 1 << 7
+        if n_bytes == 0:
+            while (mask & num):
+                n_bytes += 1
+                mask = mask >> 1
 
-        # start of new char
-        if num_bytes == 0:
-            num_bytes = get_byte_count(num)-1
-            if num_bytes is None:
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-
-        # continue processing char
         else:
-            if not num.startswith("10"):
+
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            num_bytes -= 1
+        n_bytes -= 1
 
-    return num_bytes == 0
-
-
-def get_byte_count(num):
-    """ Get byte count of character """
-    prefix_dict = {
-        "0": 1,
-        "110": 2,
-        "1110": 3,
-        "11110": 4,
-        }
-
-    for prefix in prefix_dict:
-        if num.startswith(prefix):
-            return prefix_dict[prefix]
-
-    return False
+    if n_bytes:
+        return False
+    else:
+        return True
