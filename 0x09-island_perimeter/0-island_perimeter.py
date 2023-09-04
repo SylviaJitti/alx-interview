ae8fcabfbf53569901db61e6d452fac4c8ab18f5
Sylvia Jitti <72154164+SylviaJitti@users.noreply.github.com>
2023-09-04 10:03:14 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""Island Perimeter
-"""
+""" island_perimeter function """
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid
+    """  returns the perimeter of the island described in grid """
 
-    grid: a list of list of integers
+    def edges(matrix):
+        """ detect number of edges along horizontal direction """
+        count = 0
+        for row in matrix:
+            row = [0] + row + [0]
+            for i in range(1, len(row)):
+                count += row[i] != row[i-1]
+        return count
 
-    return: perimeter of the island described in grid
-    """
-
-    perimeter = 0
-
-    # Loop through each row of the grid
+    tgrid = [[] for _ in range(len(grid[0]))]
     for row in grid:
-        # Loop through each cell in the row
-        for cell in row:
-            if cell == 1:  # If it's land
-                perimeter += 4  # Count all four sides
+        for i, item in enumerate(row):
+            tgrid[i].append(item)
 
-                # Check if there's land in the adjacent cells
-                # Subtract 1 for each adjacent land cell
-                c_index = row.index(cell)
-                r_index = grid.index(row)
-                if c_index > 0 and row[c_index - 1] == 1:
-                    perimeter -= 1
-                if c_index < len(row) - 1 and row[c_index + 1] == 1:
-                    perimeter -= 1
-                if r_index > 0 and grid[r_index - 1][c_index] == 1:
-                    perimeter -= 1
-                if r_index < len(grid) - 1 and grid[r_index + 1][c_index] == 1:
-                    perimeter -= 1
-
-    return perimeter
+    return edges(grid) + edges(tgrid)
